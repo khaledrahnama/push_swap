3109d8035b62b76e43d7c4d59e1821aa7d12d141
Khaled Rahnama <krahnama@c4a12c7.42berlin.de>
2026-07-17 17:12:20 +0200

    defined structures for stack and linked list

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,12 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct stack {
-    int content;
-    struct stack* next;
-} stack;
+
+typedef struct s_node
+{
+    int             value;
+    struct s_node   *next;
+}   t_node;
 
 #include "libft.h"
 
