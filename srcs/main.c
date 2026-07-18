b0e98e6e183f5b37dcab6f91cec2ab70de9e05f9
Khaledrahnama <khaledrahnama@users.noreply.github.com>
2026-07-18 12:03:37 +0200

    Made and display stack a and b

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,31 @@
 /*   By: khaledrahnama <khaledrahnama@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 11:54:11 by khaledrahna       #+#    #+#             */
-/*   Updated: 2026/07/18 11:54:16 by khaledrahna      ###   ########.fr       */
+/*   Updated: 2026/07/18 12:01:47 by khaledrahna      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	(void)argc;
+	t_stack	a;
+	t_stack	b;
+
 	(void)argv;
+	(void)argc;
+	stack_init(&a, 5);
+	stack_init(&b, 5);
+
+	a.data[0] = 3;
+	a.data[1] = 1;
+	a.data[2] = 2;
+	a.size = 3;
+
+	stack_print(&a, 'a');
+	stack_print(&b, 'b');
+
+	stack_free(&a);
+	stack_free(&b);
 	return (0);
 }
--- srcs/stack_utils.c
+++ srcs/stack_utils.c
@@ -6,7 +6,40 @@
 /*   By: khaledrahnama <khaledrahnama@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 11:51:52 by khaledrahna       #+#    #+#             */
-/*   Updated: 2026/07/18 11:51:53 by khaledrahna      ###   ########.fr       */
+/*   Updated: 2026/07/18 11:59:34 by khaledrahna      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	stack_init(t_stack *stack, int capacity)
+{
+	stack->data = malloc(sizeof(int) * capacity);
+	if (!stack->data)
+	{
+		write(2, "Error\n", 6);
+		exit(1);
+	}
+	stack->size = 0;
+}
+
+void	stack_free(t_stack *stack)
+{
+	free(stack->data);
+	stack->data = NULL;
+	stack->size = 0;
+}
+
+void	stack_print(t_stack *stack, char name)
+{
+	int	i;
+
+	i = 0;
+	printf("Stack %c (size %d): ", name, stack->size);
+	while (i < stack->size)
+	{
+		printf("%d ", stack->data[i]);
+		i++;
+	}
+	printf("\n");
+}
