d929d75c69898511bb119f64b7bff8a01fd347cc
Khaledrahnama <khaledrahnama@users.noreply.github.com>
2026-08-16 18:28:10 +0200

    tester added and updated parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khaledrahnama <khaledrahnama@student.42.fr>+#+  +:+       +#+        */
+/*   By: khaledrahnama <khaledrahnama@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/16 00:00:00 by khaledrahnama     #+#    #+#             */
-/*   Updated: 2026/08/16 00:00:00 by khaledrahnama    ###   ########.fr       */
+/*   Created: 2026/08/16 00:00:00 by khaledrahna       #+#    #+#             */
+/*   Updated: 2026/08/16 18:25:27 by khaledrahna      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,29 @@ static void	parse_one_number(char *joined, int *idx, t_stack *a)
 	a->data[a->size++] = value;
 }
 
+static void	check_no_blank_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	int	has_content;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		has_content = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] != ' ')
+				has_content = 1;
+			j++;
+		}
+		if (!has_content)
+			error_exit();
+		i++;
+	}
+}
+
 void	parse_args(int argc, char **argv, t_stack *a)
 {
 	char	*joined;
@@ -34,6 +57,7 @@ void	parse_args(int argc, char **argv, t_stack *a)
 		stack_init(a, 0);
 		return ;
 	}
+	check_no_blank_args(argc, argv);
 	joined = join_args(argc, argv);
 	stack_init(a, ft_strlen(joined) + 1);
 	i = 0;
@@ -45,5 +69,7 @@ void	parse_args(int argc, char **argv, t_stack *a)
 			break ;
 		parse_one_number(joined, &i, a);
 	}
+	if (a->size == 0)
+		error_exit();
 	free(joined);
 }
